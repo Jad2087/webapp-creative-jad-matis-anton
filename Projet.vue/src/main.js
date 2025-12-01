6b72db613f1ed74ce9be5ddae8aa41d163273c01
Jad2087 <2257715@cmontmorency.qc.ca>
2025-12-01 10:17:01 -0500

    formater main.js

--- Projet.vue/src/main.js
+++ Projet.vue/src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import {
+  createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 
 // Import Pinia
-import { createPinia } from 'pinia'
+import {
+  createPinia
+} from 'pinia'
 
 
 // 🔊 Import du son (depuis le dossier Sounds à la racine)
@@ -15,25 +19,22 @@ const pinia = createPinia()
 
 // 🔊 Fonction pour jouer le son
 function playClickSound() {
-    const audio = new Audio(clickSound)
-    audio.volume = 0.4 // tu peux ajuster le volume
-    audio.play().catch(() => {
-      // on ignore les petites erreurs éventuelles
-    })
-  }
-  
-  // 🔊 Écouter tous les clics de souris (bouton gauche)
-  window.addEventListener('mousedown', (event) => {
-    if (event.button === 0) { // 0 = clic gauche
-      playClickSound()
-    }
+  const audio = new Audio(clickSound)
+  audio.volume = 0.4 // tu peux ajuster le volume
+  audio.play().catch(() => {
+    // on ignore les petites erreurs éventuelles
   })
+}
+
+// 🔊 Écouter tous les clics de souris (bouton gauche)
+window.addEventListener('mousedown', (event) => {
+  if (event.button === 0) { // 0 = clic gauche
+    playClickSound()
+  }
+})
 
 // Ajoute Pinia et Router à l'app
 app.use(pinia)
 app.use(router)
 
 app.mount('#app')
-
-
-
